e163bf5f2455c24cdd7081ab56063b13fcb6a851
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2022-04-20 19:58:37 -0400

    Update HitoIII_1.py

--- Lab2/HitoIII_1.py
+++ Lab2/HitoIII_1.py
@@ -12,7 +12,7 @@ opts.add_argument("Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML
 
 Nombre = ["Camilo"]
 Apellido = ["Omori"]
-Email = ["ratonsone_z53x@hrepy.com"] #Pagina de correo temporal --> https://temp-mail.org/es/change
+Email = [""] #Pagina de correo temporal --> https://temp-mail.org/es/change
 Telefono = [""] #Inserte un numero telefonico
 Password = ["am0gus"]
 NewPassword = ["k1dam0gus"]
